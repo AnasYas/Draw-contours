bbddb178f76120fff38175108599e5195f610688
Anas Yaseen <89707493+AnasYas@users.noreply.github.com>
2021-09-14 14:01:10 +0300

    Create draw_contours_opencv.py

---
+++ draw_contours_opencv.py
@@ -0,0 +1,15 @@
+import cv2
+import  numpy as np
+img=cv2.imread('logo.png')
+img=cv2.resize(img,(512,512))
+imgray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+ret,thresh=cv2.threshold(imgray,20,255,0)
+contours,hierarchy=cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+print('number of contours= '+str(len(contours)))
+print(contours[0])
+cv2.drawContours(img,contours,-1,(0,255,0),5)
+cv2.imshow('Image',img)
+cv2.imshow('Image gray',imgray)
+cv2.imshow('thresh',thresh)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
